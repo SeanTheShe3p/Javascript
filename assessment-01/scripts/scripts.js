ea3b5d4c71ef4885b8b4cc80a78e6b2fc0399b79
SeanTheShe3p <swiddowson@students.mchenry.edu>
2023-01-23 00:07:25 -0600

    Submitted Assesment01

---
+++ assessment-01/scripts/scripts.js
@@ -0,0 +1,6 @@
+function extFunction(){document.getElementById("paragraph_main").innerHTML = 
+" Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+
+function resizeFunction(){document.getElementById("skate").style.height = "200px"};
+
+function colorFunction(){document.getElementById("paragraph_main").style.color = "lightblue"};
