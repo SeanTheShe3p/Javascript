bc5c0d037ee482deb2d6850b55a9d5e05366d455
SeanTheShe3p <swiddowson@students.mchenry.edu>
2023-02-03 19:48:31 -0600

    updated

--- assesment-03/scripts/calculate.js
+++ assesment-03/scripts/calculate.js
@@ -1,15 +1,16 @@
-const gorrilaz = {image: "assesment-03\images\gorrilaz.png",founded: "1998", members: "Murdoc Niccals, 2-D, Noodle, Russel Hobbs", bestAlbum: "DEMON DAYS", website:"www.gorillaz.com"}
-const prettyLights = {image: "assesment-03\images\prettyLights.jpg",founded: "2004", members: "Derek Vincent Smith", bestAlbum: "A Color Map of the Sun", website:"www.prettylightsmusic.com"}
-const gramatik = {image: "assesment-03\images\gramatik.jpg",founded: "2008", members: "Denis Jašarević", bestAlbum: "#digitalfreedom EP", website:"www.gramatik.net"}
-const randomRab = {image: "assesment-03\images\randomRab.jpg",founded: "2001", members: "Rab Clinton", bestAlbum: "Visurreal", website:"www.randomrab.com"}
-const desertDwellers = {image: "assesment-03\images\desertDwellers.jpg",founded: "2003", members: "Amani Friend and Treavor Moontribe", bestAlbum: "DownTemple Dub: Remixed", website:"www.desertdwellers.org/"}
+const gorrilaz = {image: "images\\gorrilaz.png",founded: "1998", members: "Murdoc Niccals, 2-D, Noodle, Russel Hobbs", bestAlbum: "DEMON DAYS", website:"www.gorillaz.com"}
+const prettyLights = {image: "images\\prettyLights.jpg",founded: "2004", members: "Derek Vincent Smith", bestAlbum: "A Color Map of the Sun", website:"www.prettylightsmusic.com"}
+const gramatik = {image: "images\\gramatik.jpg",founded: "2008", members: "Denis Jašarević", bestAlbum: "#digitalfreedom EP", website:"www.gramatik.net"}
+const randomRab = {image: "images\\randomRab.jpg",founded: "2001", members: "Rab Clinton", bestAlbum: "Visurreal", website:"www.randomrab.com"}
+const desertDwellers = {image: "images\\desertDwellers.jpg",founded: "2003", members: "Amani Friend and Treavor Moontribe", bestAlbum: "DownTemple Dub: Remixed", website:"www.desertdwellers.org/"}
 var listData = [gorrilaz,prettyLights,gramatik,randomRab,desertDwellers]
+
 function insertData(){
     for(header in listData){
-        let html = `<h2>Music</h2><ul>`;
+        var html = `<h2>Music</h2><ul>`;
  
         for(i of listData){
-            //html += `<img src="${i.image}>`;
+            html += `<img src=\"${i.image}\">`;
             html += `<li>founded: ${i.founded}</li>`;
             html += `<li>members: ${i.members}</li>`;
             html += `<li>bestAlbum: ${i.bestAlbum}</li>`;
